d91efec2890f9b64c5d30061e76a4ff45d0d29aa
VyacheslavShtyrlin <vyacheslavshtyrlin@yandex.ru>
2022-05-22 14:44:18 +0500

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 export default function Header({ userEmail, onSignOut }) {
-  const path = useLocation();
+  const {pathname} = useLocation();
   return (
     <header className="header">
       <img className="header__logo" alt="логотип" src={logo} />
-      {path.pathname === "/" && (
+      {pathname === "/" && (
         <div className="header__container">
           <span className="header__email">{userEmail.email}</span>
           <NavLink className="header__link" onClick={onSignOut} to="/sign-in">
@@ -16,12 +16,12 @@ export default function Header({ userEmail, onSignOut }) {
           </NavLink>
         </div>
       )}
-      {path.pathname === "/sign-in" && (
+      {pathname === "/sign-in" && (
         <NavLink to="/sign-up" className="header__link">
           Регистрация
         </NavLink>
       )}
-      {path.pathname === "/sign-up" && (
+      {pathname === "/sign-up" && (
         <NavLink to="/sign-in" className="header__link">
           Вход
         </NavLink>
